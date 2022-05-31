0f89aae80649a6cd8693b58e35cab74e4652a3a3
David Schöne <david.blaa@googlemail.com>
2022-05-31 15:14:16 +0200

    change index because of not used awesome fonts

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,21 +14,20 @@ import { faBars, faPlay, faDownload } from '@fortawesome/free-solid-svg-icons'
 <Container>
 
  <img src="../../static/images/Bexis_Home_Header.gif" alt="banner" width="100%"/>
- <img src="../../static/icons/bookmarks_FILL0_wght400_GRAD0_opsz48.svg" alt="banner" width="100%"/>
 
  <Row>
   <Col xs=4>
-   <Card title="Feature" icon={faBars}>
+   <Card title="Feature">
     BEXIS 2 supports a broad range of data structures, data types and more. Take a look at all <a href="/features">BEXIS 2 features</a>.
    </Card>
   </Col>
   <Col xs=4>
-   <Card title="Demo" icon={faPlay} >
+   <Card title="Demo">
     Get familiar with BEXIS 2 environment and try out its useful features. <a href="https://demo.bexis2.uni-jena.de/" target="_blank">See the latest version in action!</a>
    </Card>
   </Col>
   <Col xs=4>
-   <Card title="Latest Releases" icon={faDownload}>
+   <Card title="Latest Releases">
     Manage your research data now. BEXIS is easy to run and simple to maintain. Download the latest version of BEXIS 2.
    </Card>
   </Col>
