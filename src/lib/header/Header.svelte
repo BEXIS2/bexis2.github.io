eedb6c9d87bf87c639a0f63b4bb7fb9d87d7e87c
David Schöne <david.blaa@googlemail.com>
2022-06-01 16:57:56 +0200

    remove unused code

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -79,7 +79,7 @@
 						</DropdownMenu>
 				</Dropdown>
 
-				<NavItem>
+				<!-- <NavItem>
 					<li class="nav-item" class:active={$page.url.pathname === base + '/software/features'}>
 						<a sveltekit:prefetch href={base + '/software/features'} class="nav-link">Features</a>
 					</li>
@@ -88,71 +88,7 @@
 					<li class="nav-item" class:active={$page.url.pathname === base + '/software/releases'}>
 						<a sveltekit:prefetch href={base + '/software/releases'} class="nav-link">Releases</a>
 					</li>
-				</NavItem>
-			<!--	<NavItem>
-					<li class="nav-item" class:active={$page.url.pathname === base + '/'}>
-						<a sveltekit:prefetch href={base + '/'} class="nav-link">Home</a>
-					</li>
-				</NavItem>-->
-				<!--	<NavItem>
-				<li class="nav-item" class:active={$page.url.pathname === base + '/test'}>
-					<a sveltekit:prefetch href={base + '/test'} class="nav-link">Test Page</a>
-				</li>
-			</NavItem>-->
-			<!--	<NavItem>
-					<li class="nav-item" class:active={$page.url.pathname === base + '/learn'}>
-						<a sveltekit:prefetch href={base + '/learn'} class="nav-link">Learn</a>
-					</li>
-				</NavItem>
-				<NavItem>
-					<li class="nav-item" class:active={$page.url.pathname === base + '/explore'}>
-						<a sveltekit:prefetch href={base + '/explore'} class="nav-link">Explore</a>
-					</li>
-				</NavItem>
-				<NavItem>
-					<li class="nav-item" class:active={$page.url.pathname === base + '/analyze'}>
-						<a sveltekit:prefetch href={base + '/analyze'} class="nav-link">Analyze</a>
-					</li>
-				</NavItem>
-				<NavItem>
-					<li class="nav-item" class:active={$page.url.pathname === base + '/lifegate'}>
-						<a sveltekit:prefetch href={base + '/lifegate'} class="nav-link">LifeGate</a>
-					</li>
-				</NavItem>
-				<NavItem>
-					<li class="nav-item" class:active={$page.url.pathname === base + '/iknow'}>
-						<a sveltekit:prefetch href={base + '/iknow'} class="nav-link">Knowledge Graphs</a>
-					</li>
-				</NavItem>-->
-
-				<!--		{#if $user_store != ''}
-					<NavItem>
-						<li class="nav-item" class:active={$page.url.pathname === base + '/login'}>
-							<a sveltekit:prefetch href={base + '/login'} class="nav-link"
-								>{$user_store.username}</a
-							>
-						</li>
-					</NavItem>
-				{/if}
-				<NavItem>
-					{#if $token_store == ''}
-						<li class="nav-item" class:active={$page.url.pathname === base + '/login'}>
-							<a sveltekit:prefetch href={base + '/login'} class="nav-link">Login</a>
-						</li>
-					{:else}
-						<li class="nav-item" class:active={$page.url.pathname === base + '/logout'}>
-							<a sveltekit:prefetch href={base + '/logout'} class="nav-link">Logout</a>
-						</li>
-					{/if}
-				</NavItem>-->
-				<!--	<NavItem>
-					<li class="nav-item" class:active={$page.url.pathname === base + '/imprint'}>
-						<a sveltekit:prefetch href={base + '/imprint'} class="nav-link">Imprint</a>
-					</li>
-				</NavItem>-->
-				
-				
-			
+				</NavItem> -->
 			</Nav>
 		</Collapse>
 	</Navbar>
