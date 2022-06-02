07abe04cc34adf026afaea7e90e605dad75666e3
David Schöne <david.schoene@uni-jena.de>
2022-06-02 07:54:16 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ export default {
 			sr: false
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '' : '' ///bexis2-github-io/
+			base: process.env.NODE_ENV === 'production' ? '/bexis2-github-io' : '' ///bexis2-github-io/
 			//base: process.env.NODE_ENV === 'production' ? '' : ''
 		},
 		trailingSlash: 'always',
