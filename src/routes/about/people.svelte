f955f1e25df938cc497ffaf218d3e6575965a118
David Schöne <david.blaa@googlemail.com>
2022-06-07 13:07:52 +0200

    add former members

--- src/routes/about/people.svelte
+++ src/routes/about/people.svelte
@@ -1,19 +1,34 @@
 <script>
 
 import team from '../../data/team.json'
+import former from '../../data/formermembers.json'
 import People from '$lib/components/People.svelte';
 import {Row, Col} from 'sveltestrap'
 
 </script>
 
-<h2>People</h2>
-
-<h3>BEXIS2 Core Team</h3>
+<h2>BEXIS2 Core Team</h2>
 
 <Row>
 {#if team}
   <!-- content here -->
   {#each team as member}
+    <!-- content here -->
+    <Col><People {...member} /></Col>
+  {/each}
+{/if}
+</Row>
+
+<br>
+<hr>
+<br>
+
+<h2>Former Members</h2>
+
+<Row>
+{#if former}
+  <!-- content here -->
+  {#each former as member}
     <!-- content here -->
     <Col><People {...member} /></Col>
   {/each}
