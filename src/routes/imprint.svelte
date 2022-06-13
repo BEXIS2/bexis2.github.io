49ee919a689f0cdf68403a29b6f31ac5fc7f42bb
David Schöne <david.blaa@googlemail.com>
2022-06-13 09:43:39 +0200

    BEXIS2/bexis2-github-io#2 update imprint

--- src/routes/imprint.svelte
+++ src/routes/imprint.svelte
@@ -19,7 +19,21 @@
 </svelte:head>
 
 <div class="content">
-	
+<h1>Imprint</h1> 
+
+<h2>Publisher and editorial office</h2>
+
+Prof. Dr. Birgitta König-Ries <br>
+Friedrich-Schiller-Universität Jena <br>
+Institut für Informatik <br>
+Ernst-Abbe-Platz 2 <br>
+07743 Jena <br>
+Tel.: +49-3641-946430 <br>
+<br>
+Roman Gerlach <br>
+Löbdergraben 32 <br>
+07743 Jena <br>
+Tel.: +49-3641-948968 <br>
 </div>
 
 <style>
