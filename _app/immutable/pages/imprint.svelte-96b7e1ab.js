92fcc159735ec02983d727cdbb3775499c09849d
DavidBlaa <DavidBlaa@users.noreply.github.com>
2022-06-13 07:44:54 +0000

    deploy: 49ee919a689f0cdf68403a29b6f31ac5fc7f42bb

---
+++ _app/immutable/pages/imprint.svelte-96b7e1ab.js
@@ -0,0 +1,23 @@
+import{S as N,i as O,s as Q,k as K,e as n,t as l,a0 as W,d as b,m as L,c as a,a as U,h as i,b as X,g as w,_ as e,E as C}from"../chunks/index-f4bc5dab.js";function Y(M){let o,t,c,d,f,h,m,_,u,R,p,B,v,I,E,g,J,P,x,S,T,k,y,D,q,z,A,F,G;return{c(){o=K(),t=n("div"),c=n("h1"),d=l("Imprint"),f=K(),h=n("h2"),m=l("Publisher and editorial office"),_=l(`
+
+Prof. Dr. Birgitta K\xF6nig-Ries `),u=n("br"),R=l(`
+Friedrich-Schiller-Universit\xE4t Jena `),p=n("br"),B=l(`
+Institut f\xFCr Informatik `),v=n("br"),I=l(`
+Ernst-Abbe-Platz 2 `),E=n("br"),g=l(`
+07743 Jena `),J=n("br"),P=l(`
+Tel.: +49-3641-946430 `),x=n("br"),S=K(),T=n("br"),k=l(`
+Roman Gerlach `),y=n("br"),D=l(`
+L\xF6bdergraben 32 `),q=n("br"),z=l(`
+07743 Jena `),A=n("br"),F=l(`
+Tel.: +49-3641-948968 `),G=n("br"),this.h()},l(s){W('[data-svelte="svelte-6dhl87"]',document.head).forEach(b),o=L(s),t=a(s,"DIV",{class:!0});var r=U(t);c=a(r,"H1",{});var V=U(c);d=i(V,"Imprint"),V.forEach(b),f=L(r),h=a(r,"H2",{});var j=U(h);m=i(j,"Publisher and editorial office"),j.forEach(b),_=i(r,`
+
+Prof. Dr. Birgitta K\xF6nig-Ries `),u=a(r,"BR",{}),R=i(r,`
+Friedrich-Schiller-Universit\xE4t Jena `),p=a(r,"BR",{}),B=i(r,`
+Institut f\xFCr Informatik `),v=a(r,"BR",{}),I=i(r,`
+Ernst-Abbe-Platz 2 `),E=a(r,"BR",{}),g=i(r,`
+07743 Jena `),J=a(r,"BR",{}),P=i(r,`
+Tel.: +49-3641-946430 `),x=a(r,"BR",{}),S=L(r),T=a(r,"BR",{}),k=i(r,`
+Roman Gerlach `),y=a(r,"BR",{}),D=i(r,`
+L\xF6bdergraben 32 `),q=a(r,"BR",{}),z=i(r,`
+07743 Jena `),A=a(r,"BR",{}),F=i(r,`
+Tel.: +49-3641-948968 `),G=a(r,"BR",{}),r.forEach(b),this.h()},h(){document.title="Imprint",X(t,"class","content")},m(s,H){w(s,o,H),w(s,t,H),e(t,c),e(c,d),e(t,f),e(t,h),e(h,m),e(t,_),e(t,u),e(t,R),e(t,p),e(t,B),e(t,v),e(t,I),e(t,E),e(t,g),e(t,J),e(t,P),e(t,x),e(t,S),e(t,T),e(t,k),e(t,y),e(t,D),e(t,q),e(t,z),e(t,A),e(t,F),e(t,G)},p:C,i:C,o:C,d(s){s&&b(o),s&&b(t)}}}class $ extends N{constructor(o){super(),O(this,o,null,Y,Q,{})}}export{$ as default};
