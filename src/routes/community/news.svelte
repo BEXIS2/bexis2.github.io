71f37f65146c23fd26e841e9c59e1d408bff68e2
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2022-06-24 09:11:00 +0200

    #12 news component for detailed view

---
+++ src/routes/community/news.svelte
@@ -0,0 +1,27 @@
+<script>
+	import newsJson from '../../data/news.json';
+ 	import News from '$lib/components/News.svelte'
+	import {Row, Col, Card} from 'sveltestrap';
+</script>
+
+<svelte:head>
+ <title>News</title>
+</svelte:head>
+
+<Row>
+	{#if newsJson}
+
+		{#each newsJson as news}
+				<Col xs=12>
+					<News {...news}></News>
+				</Col>
+		{/each}
+
+	{:else}
+
+	<Col xs=12>
+		Nothing to show...
+	</Col>
+
+	{/if}
+</Row>
