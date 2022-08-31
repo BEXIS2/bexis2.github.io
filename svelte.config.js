7216221dbb93ae4c9ccf97ba7d9ebf7cfb75bbe8
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2022-08-31 11:14:01 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,7 @@
-import adapter from '@sveltejs/adapter-static';
 import path from 'path';
 
 export default {
 	kit: {
-		adapter: adapter({
-			// default options are shown
-      			pages: 'build',
-      			assets: 'build',
-      			fallback: null,
-      			precompress: false
-		}),
 		paths: {
 
 			base: process.env.NODE_ENV === 'production' ? '/bexis2-github-io' : '' ///bexis2-github-io/
