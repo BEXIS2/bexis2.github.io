8e0c3b8560103ad533b1e0f5dec84cc539580f0e
Franziska Zander <franzi82@gmx.de>
2024-09-08 23:54:06 +0200

    Create +page.svelte + add accessibility statement

---
+++ src/routes/accessibility/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
+<svelte:head>
+	<title>Erklärung zur Barrierefreiheit</title>
+</svelte:head>
+
+<h2>Erklärung zur Barrierefreiheit</h2>
+<h4>Diese Erklärung zur Barrierefreiheit gilt für die unter https://bexis2.uni-jena.de veröffentlichte Webseite</h4>
+
+<p>Im Sinne der Richtlinie (EU) 2016/2102 sind wir bemüht, unsere Websites und mobilen Anwendungen im Einklang mit den Bestimmungen des Behindertengleichstellungsgesetzes des Bundes (BGG) 
+sowie der Barrierefreien-Informationstechnik-Verordnung (BITV 2.0) zur Umsetzung der Richtlinie (EU) 2016/2102 barrierefrei zugänglich zu machen.</p>
+
+<h3>Stand der Vereinbarkeit mit den An­for­der­ungen</h3>
+<p>Die Anforderungen der Barrierefreiheit ergeben sich aus §§ 3 Absätze 1 bis 4 und 4 der BITV 2.0, die auf der Grundlage von § 12d BGG erlassen wurde. </p>
+
+<p>Die Website ist mit den zuvor genannten Anforderungen derzeit nicht vollständig vereinbar. Die Website wird aktuell hinsichtlich der Barrierefreiheit geprüft. 
+  Wir bemühen uns, alle festgestellten Probleme der Zugänglichkeit zu beheben.</p>
+
+<h3>Datum der Erstellung bzw. der letzten Aktualisierung der Erklärung</h3>
+<p>Diese Erklärung wurde am 08.09.2024 erstellt.</p>
+  
+<h3>Barrieren melden: Kontakt zu den Feedback Ansprechpartnern</h3>
+<p>siehe Imprint</p>
+
+<h3>Schlichtungsverfahren</h3>
+<p>siehe  https://www.uni-jena.de/41470/barrierefreiheit</p>
